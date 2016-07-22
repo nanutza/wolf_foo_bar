89e334d24f205523a4e9b65e6b6894445c16973f
Nan Min <nanutza@gmail.com>
2016-07-22 13:07:59 -0400

    Update with release 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -21,5 +21,27 @@ def search_array(array, value)
   end
 end
 
+
+def fib(num)
+  
+  if num == 0
+    return []
+  
+  elsif num == 1
+    return [0]
+  
+  else
+    start_array = [0, 1]
+    while start_array.length < num
+      start_array << (start_array[-1] + start_array[-2])
+    end
+  end
+  return start_array
+end
+
+
 #driver code
 search_array([42, 89, 23, 1], 89)
+  
+fib(6)
+print fib(100).last == 218922995834555169026
