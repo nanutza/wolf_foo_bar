165e564284d6a59d83cca8de4d8af429fd3f2217
Nan Min <nanutza@gmail.com>
2016-07-25 14:15:38 -0400

    add Behaviors to class Puppy - Realease 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,67 @@
+# How might you design a Puppy class? 
+# What sorts of characteristics and behavior might a Puppy class involve? 
+# Using a format similar to the one we used for Eyelashus Adorableus, 
+# come up with some characteristics and behavior for a Puppy class. 
+# Use comments to add your class design
+
+
+# PUPPY SPECIES----------------------
+# Caninus adorabilis
+
+# CHARACTERISTICS--------------------
+# Size: varies
+# Paws: 4
+# Cute: yes
+# Nose: moist
+# Tail: mostly
+# Fur_color: varies
+# Name: varies
+
+# BEHAVIOR---------------------------
+# Bark
+# Run
+# Fetch
+# Dig 
+
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+  def initialize
+    puts "Initializing puppy instance"
   end
 
+  def fetch(toy)
+  p "The puppy fetches the #{toy}"
+  end
+  
+  def speak(int)
+    int.times {puts "Woof!"}
+  end
+  
+  def roll_over
+    puts "*rolls over*"
+  end
+  
+  def dog_years(human_years)
+    dog_years = human_years * 7
+  end
+  
+  def begs(minutes,treat)
+    minutes.times {puts "*begs for #{treat} for one minute*"}
+  end
 end
 
+
+# Driver Code
+ramen = Puppy.new
+dubu = Puppy.new
+
+p ramen.class
+ramen.fetch("ball")
+p ramen == dubu
+dubu.instance_of?(Puppy)
+
+ramen.speak(3)
+ramen.roll_over
+puts "Dubu is #{dubu.dog_years(4)} dog years old."
+
+dubu.begs(2, "grapes")
