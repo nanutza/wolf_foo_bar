9700ecd19086be66b9bc7966c3712b77cddcbec0
Nan Min <nanutza@gmail.com>
2016-07-26 01:22:34 -0400

    Update Santa class with attributes - Release 1

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -10,8 +10,12 @@
 
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		p "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
@@ -26,6 +30,18 @@ end
 
 #Driver Code
 
-claus = Santa.new
+claus = Santa.new("male","Caucasian")
 claus.speak
 claus.eat_milk_and_cookies("snickerdoodle")
+
+
+# Release 1: DRIVER_CODE
+santas = [] 
+santas << Santa.new("agender", "black") 
+santas << Santa.new("female", "Latino") 
+santas << Santa.new("bigender", "white") 
+santas << Santa.new("male", "Japanese") 
+santas << Santa.new("female", "prefer not to say") 
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+p santas
