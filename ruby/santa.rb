3d63f010eb73cbad4cf75a0722e2d7a6a59f2ecd
Nan Min <nanutza@gmail.com>
2016-07-26 01:32:40 -0400

    Change an Attribute with a Method / Getter and Setter Methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -18,6 +18,33 @@ class Santa
 		@age = 0
 	end
 
+	#Release 2  -   getter and setter methods
+
+	def gender=(gender)
+		@gender = gender
+	end
+
+	def age(age)
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def celebrate_birthday
+		puts "Santa's current age is #{@age}"
+		@age += 1
+		puts " After his birthday, Santa's new age is: #{@age}"
+	end
+
+	def get_mad_at(name)
+		@reindeer_ranking.delete(name)
+		@reindeer_ranking << name
+		p @reindeer_ranking
+	end
+
+
 	def speak
 		p "Ho, ho, ho! Haaaappy holidays!" 
 	end
@@ -36,12 +63,41 @@ claus.eat_milk_and_cookies("snickerdoodle")
 
 
 # Release 1: DRIVER_CODE
-santas = [] 
-santas << Santa.new("agender", "black") 
-santas << Santa.new("female", "Latino") 
-santas << Santa.new("bigender", "white") 
-santas << Santa.new("male", "Japanese") 
-santas << Santa.new("female", "prefer not to say") 
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
-p santas
+# santas = [] 
+# santas << Santa.new("agender", "black") 
+# santas << Santa.new("female", "Latino") 
+# santas << Santa.new("bigender", "white") 
+# santas << Santa.new("male", "Japanese") 
+# santas << Santa.new("female", "prefer not to say") 
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+# p santas
+
+
+# Release 1: Refactored Code
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"] 
+example_genders.length.times do |i| 
+	santas << Santa.new(example_genders[i], example_ethnicities[i]) 
+	#p santas[i] 
+end
+
+# Release 2: Change an Attribute with a Method
+
+# Add three attribute-changing methods to your Santa class:
+	# celebrate_birthday should age Santa by one year.
+	# get_mad_at can take a reindeer's name as an argument, and move that reindeer in last place in the reindeer rankings. Vixen knows what he did.
+	# The @gender attribute should have a setter method that allows @gender to be reassigned from outside the class definition.
+	# Add two "getter" methods as well:
+# The method age should simply return @age.
+# The method ethnicity should return @ethnicity.
+# Update your driver code to test your work.
+
+
+claus.celebrate_birthday
+claus.get_mad_at("Vixen")
+p claus.gender
+p claus.ethnicity
+
