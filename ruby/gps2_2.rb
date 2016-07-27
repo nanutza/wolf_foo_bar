6aebe46c3446f0862b99561fcfc351df8c0f798f
Nan Min <nanutza@gmail.com>
2016-07-26 21:35:19 -0400

    Create Eletronic shopping list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,30 @@
+# GPS 2.2 - Create an Electronic Grocery List
+
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # "read the items" 
+  # set default quantity
+  # store the items 
+  # print the list to the console "p" visualize without making it nil
+# output: hash
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: create a key and assign its value
+# output: hash
+
+# Method to remove an item from the list
+# input: ask which item?
+# steps: used built in delete method to delete the key
+# output: hash
+
+# Method to update the quantity of an item
+# input: ask which key and update the value
+# steps: within the assigned key update value
+# output: hash
+
+# Method to print a list and make it look pretty
+# input: our wondeful hash
+# steps: iterate through the hash
+# output: all the key-value pairs.
