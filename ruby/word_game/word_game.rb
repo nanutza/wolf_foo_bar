16be3c85f9ba5ad33c448fca82deee44a067359f
Nan Min <nanutza@gmail.com>
2016-07-31 02:59:44 -0400

    Refactor Word_Game and Fix Bug

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -2,7 +2,7 @@ require_relative 'word_bank'
 
 class WordGame
 attr_reader :word_bank 
-attr_accessor :game_over, :guess_count, :word, :console
+attr_accessor :game_over, :word 
 
 def initialize
 	@word = word 
@@ -12,61 +12,64 @@ def initialize
 	@game_over = false
 	@console = []
 	@max_guesses
+	@correct_letter = []
 end
 
 def play_game(word, letter)
 	print_console(word)
-
 		unless @guesses.include?(letter)
 			@guesses << letter
-			letter
 		  	count_guesses(word,letter)
 		end
-	   check_letter(word,letter)
+	check_letter(word,letter)
 end
 
 def check_letter(word,letter)
+
 	 idx = 0
-    if word.include?(letter)
+    if word.include?(letter) && !@correct_letter.include?(letter)
     	word.each do 
     		if word[idx] == letter
     		   @console[idx]= letter
+    		   @correct_letter << letter
     		end
     	idx += 1
     	end	
-
-    	puts "\nYay! The letter #{letter} is in the word."
-    	print "#{@console.join('')}\n"
+   
+		    	puts "\nYay! The letter '#{letter}' is in the word."
+		    	print "     #{@console.join('')}\n\n"
 
     	if @console == word
     		game_over(word)
     	end
-    	@console
 
     else
-        puts "The letter #{letter} is not in the word. You have #{@max_guesses - @guess_count} guesses left!"
+        puts "The letter '#{letter}' is not in the word. You have #{@max_guesses - @guess_count} guesses left!"
     end
 end
 
 def count_guesses(word,letter)
-	max = max_guesses(word)
-	if @guess_count < max - 1
+
+	if @guess_count < max_guesses(word) - 1
 	    @guess_count += 1
-		@guess_count
+
 	else
-	  @game_over = true
-	  puts "\nGame Over. You reached the maximum guess attempts!"
-	  puts "The right word is '#{word.join("")}'!"
-	  exit
+	  	@game_over = true
+	  	  25.times{print " . "}
+		  puts "\n\n     GAME OVER. You've reached the maximum guess attempts!"
+		  puts "     The right word is '#{word.join("")}'!\n\n"
+		  25.times{print " . "}
+		exit
 	end
 end
 
 def game_over(word)
+
     	@game_over = true
-    	20.times{print " * "}
-	    puts "\nCongratulations!!  You won!! The correct word is '#{word.join("")}'!"
-		puts "You guessed correctly in #{@guess_count} attempts!"
-		20.times{print " * "}
+    	25.times{print " * "}
+	    puts "\n\n     CONGRATULATIONS!!  You won!! The correct word is '#{word.join("")}'!"
+		puts "     You guessed correctly in #{@guess_count} attempts!\n\n"
+		25.times{print " * "}
     	exit
 end
 
@@ -78,13 +81,12 @@ end
 def print_console(word)
 	max_guesses(word)
 	if @guesses == []
-	word.length.times do
-		@console << " - "
+		word.length.times do
+		@console << " _ "
 	end
-	puts "Clue: The word is #{word.length} letters long."
-	puts " You have a maximum of #{@max_guesses} attempts to guess!"
-    print "#{@console.join('')}\n"
-	@console
+		puts "Clue: The word is #{word.length} letters long."
+		puts " You have a maximum of #{@max_guesses} attempts to guess!"
+	    print "\n     #{@console.join('')}\n"
 	end
 end
 
