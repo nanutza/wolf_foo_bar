dda1c48db04ca34c29b1b8efd84622377084cdc6
Nan Min <nanutza@gmail.com>
2016-07-30 23:48:06 -0400

    Finished word guessing game, including max guess limit, user feedback and final message. Update Rspec file

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -1,44 +1,28 @@
 require_relative 'word_bank'
 
 class WordGame
-attr_reader :word_bank
+attr_reader :word_bank 
 attr_accessor :game_over, :guess_count, :word, :console
 
 def initialize
-	# @word_bank = word_bank
 	@word = word 
 	@guesses = []
 	@guess_count = 0
 	@word_bank = word_bank
 	@game_over = false
 	@console = []
-
-def max_guesses(word)
-	(word.length * 2) + 2
+	@max_guesses
 end
 
-def count_guesses(word,letter)
-	max = max_guesses(word)
-	if @guess_count <= max - 1
-	    @guess_count += 1
-		@guess_count
-	else
-	  @game_over = true
-	  puts "Game Over"
-	  exit
-	end
-end
+def play_game(word, letter)
+	print_console(word)
 
-def print_console(word)
-	if @guesses == []
-	word.length.times do
-		@console << " - "
-	end
-    print "#{@console}\n"
-	@console
-	end
-end
-	puts 
+		unless @guesses.include?(letter)
+			@guesses << letter
+			letter
+		  	count_guesses(word,letter)
+		end
+	   check_letter(word,letter)
 end
 
 def check_letter(word,letter)
@@ -50,62 +34,73 @@ def check_letter(word,letter)
     		end
     	idx += 1
     	end	
-    	print "#{@console}\n"
+
+    	puts "\nYay! The letter #{letter} is in the word."
+    	print "#{@console.join('')}\n"
+
+    	if @console == word
+    		game_over(word)
+    	end
     	@console
+
     else
-        puts "The letter #{letter} is not in the word"
+        puts "The letter #{letter} is not in the word. You have #{@max_guesses - @guess_count} guesses left!"
     end
 end
 
-def play_game(word, letter)
-	p count_guesses(word,letter) 
-	p max_guesses(word)
-	print_console(word)
-	unless @guesses.include?(letter)
-		@guesses << letter
-		letter
-	  	p count_guesses(word,letter)
+def count_guesses(word,letter)
+	max = max_guesses(word)
+	if @guess_count < max - 1
+	    @guess_count += 1
+		@guess_count
+	else
+	  @game_over = true
+	  puts "\nGame Over. You reached the maximum guess attempts!"
+	  puts "The right word is '#{word.join("")}'!"
+	  exit
 	end
-   check_letter(word,letter)
-  
 end
+
+def game_over(word)
+    	@game_over = true
+    	20.times{print " * "}
+	    puts "\nCongratulations!!  You won!! The correct word is '#{word.join("")}'!"
+		puts "You guessed correctly in #{@guess_count} attempts!"
+		20.times{print " * "}
+    	exit
+end
+
+def max_guesses(word)
+	@max_guesses = (word.length * 2) + 5
+end
+
+
+def print_console(word)
+	max_guesses(word)
+	if @guesses == []
+	word.length.times do
+		@console << " - "
+	end
+	puts "Clue: The word is #{word.length} letters long."
+	puts " You have a maximum of #{@max_guesses} attempts to guess!"
+    print "#{@console.join('')}\n"
+	@console
+	end
+end
+
 
 end
 
 
 # driver code
+word = @word_bank.sample.delete("\n").delete("\t").split('')
 
-# word = @word_bank.sample.delete("\n").delete("\t")
-# user interface
-
-
-
-# word = @word_bank.sample.delete("\n").delete("\t")
-word = "hello".split('')
 game = WordGame.new
-p word
-game.play_game(word, "o")
-game.play_game(word, "e")
-game.play_game(word, "b")
-game.play_game(word, "l")
-game.play_game(word, "u")
-game.play_game(word, "h")
-game.play_game(word, "m")
-game.play_game(word, "p")
-game.play_game(word, "y")
-game.play_game(word, "d")
-game.play_game(word, "f")
-game.play_game(word, "g")
-game.play_game(word, "h")
-game.play_game(word, "j")
-game.play_game(word, "k")
-# game.play_game(word, "l")
-# game.play_game(word, "n")
-# game.play_game(word, "q")
-# game.play_game(word, "r")
-# game.play_game(word, "s")
-# game.play_game(word, "t")
-# game.play_game(word, "v")
-# game.play_game(word, "x")
-# game.play_game(word, "z")
+
+	loop do
+		letter = ("abcdefghijklmnopqrstuvwxyz").split('').sample
+		game.play_game(word, letter)
+		break if @game_over
+	end
+
 
--- ruby/word_game/word_game_spec.rb
+++ ruby/word_game/word_game_spec.rb
@@ -3,17 +3,14 @@ require_relative 'word_game'
 describe Game do 
 	let(:game) {Game.new}
 
-  it "has a readable name" do
+  it "has a readable writable and variable" do
     expect(game.game_over).to eq "false"
   end
 	
-
-
-
-
-
-
-
-
+  it "can play the game" do
+    game.play_game("hello", "o")
+    expect(game.play_game("hello", "o")).to eq [" - "," - "," - "," - "," o "]
+    expect(game.max_guesses("hello")).to eq 15
+  end
 
 end
