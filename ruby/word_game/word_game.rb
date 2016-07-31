98efcd09d45f55d3e2a1ee7cab908d6e620c6cfd
Nan Min <nanutza@gmail.com>
2016-07-30 21:04:58 -0400

    Created initial prototype of word game and initial Rspec test file

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -7,14 +7,14 @@ attr_accessor :game_over, :guess_count, :word, :console
 def initialize
 	# @word_bank = word_bank
 	@word = word 
-	@guesses = ""
+	@guesses = []
 	@guess_count = 0
 	@word_bank = word_bank
 	@game_over = false
-	@console = ""
+	@console = []
 
 def max_guesses(word)
-	max = (word.length * 2) + 2
+	(word.length * 2) + 2
 end
 
 def count_guesses(word,letter)
@@ -24,39 +24,50 @@ def count_guesses(word,letter)
 		@guess_count
 	else
 	  @game_over = true
+	  puts "Game Over"
 	  exit
 	end
 end
 
 def print_console(word)
-	@console = word.length.times {print " - "}
+	if @guesses == []
+	word.length.times do
+		@console << " - "
+	end
+    print "#{@console}\n"
+	@console
+	end
 end
 	puts 
 end
 
-def play_game(word, letter)
-	print_console(word)
-	unless @guesses.include?(letter)
-		@guesses << letter
-	  	#p count_guesses(word,letter)
+def check_letter(word,letter)
+	 idx = 0
     if word.include?(letter)
-    	idx = 0
-    	word.each_char do 
+    	word.each do 
     		if word[idx] == letter
-    		   @console[idx] = letter
-    		else
-    			@console[idx] = " - "
+    		   @console[idx]= letter
     		end
     	idx += 1
     	end	
-    	print @console
+    	print "#{@console}\n"
     	@console
     else
-        p "no"
+        puts "The letter #{letter} is not in the word"
     end
-    end
-   
-    p @guesses
+end
+
+def play_game(word, letter)
+	p count_guesses(word,letter) 
+	p max_guesses(word)
+	print_console(word)
+	unless @guesses.include?(letter)
+		@guesses << letter
+		letter
+	  	p count_guesses(word,letter)
+	end
+   check_letter(word,letter)
+  
 end
 
 end
@@ -65,25 +76,20 @@ end
 # driver code
 
 # word = @word_bank.sample.delete("\n").delete("\t")
-
-
-
 # user interface
 
 
 
-word = @word_bank.sample.delete("\n").delete("\t")
+# word = @word_bank.sample.delete("\n").delete("\t")
+word = "hello".split('')
 game = WordGame.new
 p word
-game.play_game(word, "a")
+game.play_game(word, "o")
 game.play_game(word, "e")
-game.play_game(word, "o")
-game.play_game(word, "o")
-game.play_game(word, "o")
 game.play_game(word, "b")
-game.play_game(word, "i")
+game.play_game(word, "l")
 game.play_game(word, "u")
-game.play_game(word, "c")
+game.play_game(word, "h")
 game.play_game(word, "m")
 game.play_game(word, "p")
 game.play_game(word, "y")
@@ -93,13 +99,13 @@ game.play_game(word, "g")
 game.play_game(word, "h")
 game.play_game(word, "j")
 game.play_game(word, "k")
-game.play_game(word, "l")
-game.play_game(word, "n")
-game.play_game(word, "q")
-game.play_game(word, "r")
-game.play_game(word, "s")
-game.play_game(word, "t")
-game.play_game(word, "v")
-game.play_game(word, "x")
-game.play_game(word, "z")
+# game.play_game(word, "l")
+# game.play_game(word, "n")
+# game.play_game(word, "q")
+# game.play_game(word, "r")
+# game.play_game(word, "s")
+# game.play_game(word, "t")
+# game.play_game(word, "v")
+# game.play_game(word, "x")
+# game.play_game(word, "z")
 
