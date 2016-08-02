ac650436403716c842bef06d9a3e63691abdb747
Nan Min <nanutza@gmail.com>
2016-08-01 23:05:28 -0400

    Added Reflections

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,13 +1,14 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [with: Wellington - guide  ].
+# We spent [1.5] hours on this challenge.
 
 # EXPLANATION OF require_relative
 #
 #
 require_relative 'state_data'
 #link with another file the directory declaring the relative path.
+#require needs the full path or url
 
 class VirusPredictor
 
@@ -20,28 +21,35 @@ class VirusPredictor
 
 # this method calls the other two private methods: predicted_deaths, speed_of_spread
   def virus_effects
-    predicted_deaths
-    speed_of_spread
+    number_of_deaths = predicted_deaths
+    speed = speed_of_spread
+    print_results(number_of_deaths, speed)
+
   end
 
   private
+
+  def print_results(number_of_deaths, speed)
+    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    puts " and will spread across the state in #{speed} months.\n\n"
+  end
 
 # this method takes three parameter and returns number_of_deaths
   def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      (@population * 0.4).floor
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      (@population * 0.3).floor
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      (@population * 0.2).floor
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      (@population * 0.1).floor
     else
-      number_of_deaths = (@population * 0.05).floor
+      (@population * 0.05).floor
     end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    # print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
@@ -53,18 +61,18 @@ class VirusPredictor
     speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      0.5
     elsif @population_density >= 150
-      speed += 1
+      1
     elsif @population_density >= 100
-      speed += 1.5
+      1.5
     elsif @population_density >= 50
-      speed += 2
+      2
     else
-      speed += 2.5
+      2.5
     end
 
-    puts " and will spread across the state in #{speed} months.\n\n"
+    # puts " and will spread across the state in #{speed} months.\n\n"
 
   end
 
@@ -98,6 +106,25 @@ STATE_DATA.each do |state, hash|
 end
 
 
-
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# The hash rocket is used for strings                -  hash = {"key" => value}
+# in the simplified syntax the key becomes a symbol  -  hash = {key : value}
+
+# What does require_relative do? How is it different from require?
+# require_relative references a file within the same directory.
+# require needs to have a full path to the file being used.
+
+# What are some ways to iterate through a hash?
+# Using .each you can iterate through keys, values, or both keys and values.
+
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+# The instance variables were already called through the initialize
+# method so they were already available to methods within the class, and it was not necessary to pass them as arguments.
+
+# What concept did you most solidify in this challenge? 
+# 1) the two syntaxes to represent a hash: and the usefulness of symbols
+# 2) the scope of variables
+# 3) new: the introduction of "private methods" and "Constants" as global elements.
