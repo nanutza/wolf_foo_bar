c93c1a2d8b5355ffa4a12256103afec9fe3633dc
Nan Min <nanutza@gmail.com>
2016-08-06 15:07:11 -0400

    Create JS data structure file that create two arrays and an object

---
+++ js/data_structures.js
@@ -0,0 +1,15 @@
+// 7.2 JS data Structures
+// Release 0 declare 2 separate arrays
+// release 1 create an object
+
+var color = ["red", "green", "yellow", "brown"];
+var name = ["Ed", "Bob", "Linda", "Jane"];
+
+var horse = {name: "name", color: "color"};
+
+for (var i = 0; i < name.length; i++) {
+  console.log(name[i] = color[i]);
+} 
+
+
+console.log(name)
