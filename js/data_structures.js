274fb03f78ffa984807178d1f0f5a59bb35df3e9
Nan Min <nanutza@gmail.com>
2016-08-06 15:19:34 -0400

    Write a constructor function that build a car and give it different properties and a function

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,5 +11,38 @@ for (var i = 0; i < name.length; i++) {
   console.log(name[i] = color[i]);
 } 
 
-
 console.log(name)
+
+// Release 2:  write a constructor function for a car. 
+// Give it a few different properties of various data types, including at least one function. 
+// Demonstrate that your function works by creating a few cars with it.
+
+var car = {name: 'Honda', age: 3, isReliable: true};
+
+function Car(name, age, isReliable) {
+
+  console.log("Our new car:", this);
+  
+
+  this.name = name;
+  this.age = age;
+  this.isReliable = isReliable;
+
+  this.drive = function() { console.log("Vroom!"); };
+  
+  console.log("CAR INITIALIZATION COMPLETE");
+}
+
+console.log("Let's build a car ...");
+
+var anotherCar = new Car("Toyota", 8, true);
+console.log(anotherCar);
+console.log("Our New car is amazing:");
+anotherCar.drive();
+console.log("----");
+
+console.log("Let's build another car ...");
+var yetAnotherCar = new Car("Ford", 2, false);
+console.log(yetAnotherCar.name);
+console.log("This car is a wreck");
+console.log("----")
