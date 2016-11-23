700d72311868851eb9184eb73f9c7aab391dbb3c
Nan Min <nanutza@gmail.com>
2016-11-23 18:00:50 -0500

    data visualization

--- lib/seeds/csv_to_geojson.rb
+++ lib/seeds/csv_to_geojson.rb
@@ -1,5 +1,4 @@
 # data=NET::HTTP.get(URI.parse('http://datamine.mta.info/mta_esi.php?key=5a44f5292fb0076e8f17017858ce3c58'))
-
 require 'csv'
 require 'json'
 
@@ -11,7 +10,8 @@ shape_id = nil
 
 # ONLY FOR SHAPES.txt IN THE SAME FILE
 
-CSV.foreach('shapes.txt', headers: true) do |row|
+CSV.foreach('shapes_1.txt', headers: true) do |row|
+
   if row['shape_id'] != shape_id
     # new shape!
     if shape_id
@@ -28,9 +28,7 @@ CSV.foreach('shapes.txt', headers: true) do |row|
     shape_id = row['shape_id']
   end
 
-  coordinates << [row['shape_pt_lon'].to_f, row['shape_pt_lat'].to_f]
-end
-
-File.open('shapes.json', 'w') do |f|
+File.open('shapes_1.json', 'w') do |f|
+  coordinates << [row['shape_pt_lat'].to_f, row['shape_pt_lon'].to_f]
   f.write(JSON.pretty_generate({type: 'FeatureCollection', features: features}))
 end
